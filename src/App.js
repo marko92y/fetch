ecae56b1b6e4e110a6e31ed1013f896ce9d704e8
Marko <markoreact@gmail.com>
2019-11-19 14:50:54 +0100

    100%

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+
+const initProfile = {
+  followers: null,
+  publicRepos: null
+};
+
+function App() {
+
+  const [profile, setProfile] = useState(initProfile);
+
+  async function getProfile() {
+    const response = await fetch('https://api.github.com/users/markoreact');
+    const json = await response.json();
+
+    setProfile({
+      followers:json.followers,
+      publicRepos:json.public_repos
+    })
+  }
+  
+  useEffect(()=> {
+    getProfile();
+  }, [])
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Fetch Data</h2>
+        <h3>{`follower: ${profile.followers}, repos: ${profile.publicRepos} `}</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
